1561546dc46e40ed5d47ba17e6eb6dab11a3a6b5
ght_sky <ght360427851@163.com>
2024-10-22 13:37:12 +0800

    pytorch学习_day2

---
+++ torch_study/transform_1.py
@@ -0,0 +1,18 @@
+import PIL.Image
+from torchvision import transforms
+
+# toTenor
+
+img_path = "data/hymenoptera_data/train/ants/0013035.jpg"
+
+img_PIL = PIL.Image.open(img_path)
+
+tensor_trans = transforms.ToTensor()
+
+tensor_img = tensor_trans(img_PIL)
+
+print(tensor_img)
+
+
+
+
