80e0d014dc46a5e1e63cd078ca6443b061857c3a
ght_sky <ght360427851@163.com>
2024-10-22 13:49:26 +0800

    pytorch学习_day2

--- torch_study/pytorch/transform_1.py
+++ torch_study/pytorch/transform_1.py
@@ -1,18 +1,19 @@
 import PIL.Image
+import cv2
 from torchvision import transforms
 
 # toTenor
 
 img_path = "../data/hymenoptera_data/train/ants/0013035.jpg"
 
-img_PIL = PIL.Image.open(img_path)
-
 tensor_trans = transforms.ToTensor()
+
+#PIL类型
+img_PIL = PIL.Image.open(img_path)
 
 tensor_img = tensor_trans(img_PIL)
 
-print(tensor_img)
+#ndarray类型
+img_cv = cv2.imread(img_path)
 
-
-
-
+tensor_img2 = tensor_trans(img_cv)
